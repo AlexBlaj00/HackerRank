58f685a1820137282d861274bdfed9957867e996
Alex Blaj <alex.blaj00@e-uvt.ro>
2021-07-08 14:52:03 +0300

    problema 5

---
+++ src/Pb5.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+import java.util.*;
+
+class Sports{
+    String getName(){
+        return "Generic Sports";
+    }
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Sports sport = new Sports();
+        Soccer soccer = new Soccer();
+
+        System.out.println(sport.getName());
+        sport.getNumberOfTeamMembers();
+
+        System.out.println(soccer.getName());
+        soccer.getNumberOfTeamMembers();
+    }
+}
